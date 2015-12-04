1eb9cda5ae3fd6aeed4db4c641c507efee1b0685
Helmut Ye <helmut.ye@yoozi.cn>
2015-12-05 00:57:53 +0800

    category

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Database\Seeder;
 
-// composer require laracasts/testdummy
-use Laracasts\TestDummy\Factory as TestDummy;
 
 class CategoryTableSeeder extends Seeder
 {
     public function run()
     {
 
-       $categories = ['Women\'s Clothing',
+        DB::table('categories')->delete();
+
+        $categories = ['Women\'s Clothing',
             'Men\'s Clothing',
             'Travel & Vacations',
             'Health & Beauty',
