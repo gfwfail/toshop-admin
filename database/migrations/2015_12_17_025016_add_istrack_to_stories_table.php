5810049937610b78559c994aecaf23b92e16c40e
Helmut Ye <helmut.ye@yoozi.cn>
2015-12-17 11:43:05 +0800

    istrack

---
+++ database/migrations/2015_12_17_025016_add_istrack_to_stories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIstrackToStoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('stories', function (Blueprint $table) {
+            $table->boolean('istrack');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('stories', function (Blueprint $table) {
+            $table->dropColumn('istrack');
+        });
+    }
+}
