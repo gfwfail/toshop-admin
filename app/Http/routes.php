8908efcd6ee9648505706d5723930ebdd5c9b421
Helmut Ye <helmut.ye@yoozi.cn>
2015-12-17 13:12:55 +0800

    target

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -43,20 +43,21 @@ Route::group(['namespace'=>'Frontend'], function() {
 
 </head>
 
-<body><b>
-<B><a href="http://www.dpbolvw.net/click-7940418-11173999-1440086582000">最便宜机票，降价了退差价</a><img src="http://www.ftjcfx.com/image-7940418-11173999-1440086582000" width="1" height="1" border="0"/>
+<body>
+<B><a href="http://www.dpbolvw.net/click-7940418-11173999-1440086582000" target=_blank>最便宜机票，降价了退差价</a><img src="http://www.ftjcfx.com/image-7940418-11173999-1440086582000" width="1" height="1" border="0"/>
 <BR>
-<a href="http://click.linksynergy.com/fs-bin/click?id=XRP5K7ZpX*A&offerid=289612&type=3&subid=0">惠普电脑甩卖</a><IMG border=0 width=1 height=1 src="http://ad.linksynergy.com/fs-bin/show?id=XRP5K7ZpX*A&bids=289612&type=3&subid=0">
+<a href="http://click.linksynergy.com/fs-bin/click?id=XRP5K7ZpX*A&offerid=289612&type=3&subid=0" target=_blank>惠普电脑甩卖</a><IMG border=0 width=1 height=1 src="http://ad.linksynergy.com/fs-bin/show?id=XRP5K7ZpX*A&bids=289612&type=3&subid=0">
 <BR>
-<a href="http://click.linksynergy.com/fs-bin/click?id=XRP5K7ZpX*A&offerid=407990&type=3&subid=0">BestBuy大降价 </a><IMG border=0 width=1 height=1 src="http://ad.linksynergy.com/fs-bin/show?id=XRP5K7ZpX*A&bids=407990&type=3&subid=0">
+<a href="http://click.linksynergy.com/fs-bin/click?id=XRP5K7ZpX*A&offerid=407990&type=3&subid=0" target=_blank>BestBuy大降价 </a><IMG border=0 width=1 height=1 src="http://ad.linksynergy.com/fs-bin/show?id=XRP5K7ZpX*A&bids=407990&type=3&subid=0">
 <BR>
-<a href="http://www.jdoqocy.com/click-7940418-10816634-1430843914000?cm_mmc=CJ-_-4647058-_-7940418-_-Save%20BIG%20on%20Walt%20Disney%20World%20Tickets!">迪斯尼乐园</a>
+<a href="http://www.jdoqocy.com/click-7940418-10816634-1430843914000?cm_mmc=CJ-_-4647058-_-7940418-_-Save%20BIG%20on%20Walt%20Disney%20World%20Tickets!" target=_blank>迪斯尼乐园票价大优惠</a>
 <BR>
-<a href="http://www.jdoqocy.com/click-7940418-10802866-1430843951000?cm_mmc=CJ-_-4647058-_-7940418-_-Carrot%20Top%20Special%20Offer!%20Seat%20Upgrade%20Package!">拉斯维加斯演出</a>
+<a href="http://www.jdoqocy.com/click-7940418-10802866-1430843951000?cm_mmc=CJ-_-4647058-_-7940418-_-Carrot%20Top%20Special%20Offer!%20Seat%20Upgrade%20Package!" target=_blank>拉斯维加斯演出</a>
 <BR>
-<a href="http://www.dpbolvw.net/click-7940418-12384462-1446675470000" target="_top">便宜！美国Fox租车</a><img src="http://www.ftjcfx.com/image-7940418-12384462-1446675470000" width="1" height="1" border="0"/>
-<BR><a href="http://www.tkqlhce.com/click-7940418-10816254-1434580419000">游玩纽约</a>
-<BR><a href=http://www.aboutairportparking.com/?affiliate_id=2034&bf=6b1f>机场停车</a>
+<a href="http://www.dpbolvw.net/click-7940418-12384462-1446675470000"  target=_blank>便宜！美国Fox租车</a><img src="http://www.ftjcfx.com/image-7940418-12384462-1446675470000" width="1" height="1" border="0"/>
+<BR><a href="http://www.tkqlhce.com/click-7940418-10816254-1434580419000" target=_blank>游玩纽约便宜票价</a>
+<BR><a href=http://www.aboutairportparking.com/?affiliate_id=2034&bf=6b1f target=_blank>机场停车</a>
+
 </b>
 </body>
 </html>';
