cb16abe7e2872c8615cb350642e6baba3eb1d975
Helmut Ye <helmut.ye@yoozi.cn>
2015-12-18 13:33:21 +0800

    ad page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -49,21 +49,22 @@ Route::group(['namespace'=>'Frontend'], function() {
 </head>
 
 <body>
-<B><a href="http://www.dpbolvw.net/click-7940418-11173999-1440086582000" target=_blank>最便宜机票，降价了退差价</a><img src="http://www.ftjcfx.com/image-7940418-11173999-1440086582000" width="1" height="1" border="0"/>
+<a href=http://www.kqzyfj.com/click-7940418-12226380-1432227120000 target=_blank>VPN翻墙服务</a>-<a href=http://www.dpbolvw.net/click-7940418-12183666-1429021479000 target=_blank>MAC好帮手</a>
 <BR>
-<a href="http://click.linksynergy.com/fs-bin/click?id=XRP5K7ZpX*A&offerid=289612&type=3&subid=0" target=_blank>惠普电脑甩卖</a><IMG border=0 width=1 height=1 src="http://ad.linksynergy.com/fs-bin/show?id=XRP5K7ZpX*A&bids=289612&type=3&subid=0">
+<a href="http://www.dpbolvw.net/click-7940418-11173999-1440086582000" target=_blank>机票</a><img src="http://www.ftjcfx.com/image-7940418-11173999-1440086582000" width="1" height="1" border="0"/>-<a href=http://www.jdoqocy.com/click-7940418-12394396-1446739210000 target=_blank>希尔顿</a>-<a href=http://www.tkqlhce.com/click-7940418-10607057-1419355758000 target=_blank>酒店特价</a>-<a href=http://www.anrdoezrs.net/click-7940418-11348741-1365723425000 target=_blank>民宿</a>
 <BR>
-<a href="http://click.linksynergy.com/fs-bin/click?id=XRP5K7ZpX*A&offerid=407990&type=3&subid=0" target=_blank>BestBuy大降价 </a><IMG border=0 width=1 height=1 src="http://ad.linksynergy.com/fs-bin/show?id=XRP5K7ZpX*A&bids=407990&type=3&subid=0">
+<a href= http://www.jdoqocy.com/click-7940418-12152605-1430344845000 target=_blank>精品鞋折扣</a>-<a href=http://www.tkqlhce.com/click-7940418-10833203-1288384463000 target=_blank>品牌珠宝</a>
 <BR>
-<a href="http://www.jdoqocy.com/click-7940418-10816634-1430843914000?cm_mmc=CJ-_-4647058-_-7940418-_-Save%20BIG%20on%20Walt%20Disney%20World%20Tickets!" target=_blank>迪斯尼乐园票价大优惠</a>
+<a href=http://www.anrdoezrs.net/click-7940418-12142593-1430330079000 target=_blank>多语言进修</a>-<a href=http://www.anrdoezrs.net/click-7940418-11588835-1425315629000 target=_blank>旧书网</a>
 <BR>
-<a href="http://www.jdoqocy.com/click-7940418-10802866-1430843951000?cm_mmc=CJ-_-4647058-_-7940418-_-Carrot%20Top%20Special%20Offer!%20Seat%20Upgrade%20Package!" target=_blank>拉斯维加斯演出</a>
+<a href=http://www.tkqlhce.com/click-7940418-11135370-1346945113000 target=_blank>Longly Planet</a>-<a href=http://www.jdoqocy.com/click-7940418-5656118-1441316733000 target=_blank>节日鲜花</a>-<a href=http://www.jdoqocy.com/click-7940418-10543206-1399309862000 target=_blank>送好礼</a>
 <BR>
-<a href="http://www.dpbolvw.net/click-7940418-12384462-1446675470000"  target=_blank>便宜！美国Fox租车</a><img src="http://www.ftjcfx.com/image-7940418-12384462-1446675470000" width="1" height="1" border="0"/>
-<BR><a href="http://www.tkqlhce.com/click-7940418-10816254-1434580419000" target=_blank>游玩纽约便宜票价</a>
-<BR><a href=http://www.aboutairportparking.com/?affiliate_id=2034&bf=6b1f target=_blank>机场停车</a>
-
-</b>
+<a href="http://click.linksynergy.com/fs-bin/click?id=XRP5K7ZpX*A&offerid=289612&type=3&subid=0" target=_blank>惠普</a><IMG border=0 width=1 height=1 src="http://ad.linksynergy.com/fs-bin/show?id=XRP5K7ZpX*A&bids=289612&type=3&subid=0">-<a href="http://click.linksynergy.com/fs-bin/click?id=XRP5K7ZpX*A&offerid=407990&type=3&subid=0" target=_blank>BestBuy</a><IMG border=0 width=1 height=1 src="http://ad.linksynergy.com/fs-bin/show?id=XRP5K7ZpX*A&bids=407990&type=3&subid=0">
+<BR>
+<a href="http://www.jdoqocy.com/click-7940418-10816634-1430843914000?cm_mmc=CJ-_-4647058-_-7940418-_-Save%20BIG%20on%20Walt%20Disney%20World%20Tickets!" target=_blank>迪斯尼优惠</a>-<a href="http://www.jdoqocy.com/click-7940418-10802866-1430843951000?cm_mmc=CJ-_-4647058-_-7940418-_-Carrot%20Top%20Special%20Offer!%20Seat%20Upgrade%20Package!" target=_blank>拉斯维加斯特价秀</a>
+<BR>
+<a href="http://www.tkqlhce.com/click-7940418-10816254-1434580419000" target=_blank>纽约游門票</a>-<BR><a href=http://www.aboutairportparking.com/?affiliate_id=2034&bf=6b1f target=_blank>机场停车</a>-<a href="http://www.dpbolvw.net/click-7940418-12384462-1446675470000"  target=_blank>租车</a><img src="http://www.ftjcfx.com/image-7940418-12384462-1446675470000" width="1" height="1" border="0"/>
+<BR><a target="_blank" rel="nofollow" href="http://www.amazon.com/b?_encoding=UTF8&camp=1789&creative=9325&linkCode=ur2&node=172282&site-redirect=&tag=toshopnet-20&linkId=DIHIJN4MVA3LIUXW" target=_blank><Amazon節日優惠</a><img src="http://ir-na.amazon-adsystem.com/e/ir?t=toshopnet-20&l=ur2&o=1" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
 </body>
 </html>';
 
