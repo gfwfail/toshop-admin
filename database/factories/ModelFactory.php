52f3b77e36beb2713e569874e8f7d3016f2d74f3
Helmut Ye <helmut.ye@yoozi.cn>
2016-01-05 14:16:06 +0800

    a

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -17,6 +17,6 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' =>  str_random(10),
         'password' => str_random(10),
         'remember_token' => str_random(10),
-        'referrer'=>App\User::count()?App\User::where('id','<','3')->get()->random()->id:0
+        'referrer'=>App\User::count()?App\User::all()->random()->id:0
     ];
 });
