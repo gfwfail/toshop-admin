1565278b7ae453bbe31d45a32fbe0d39c7c192ba
Helmut Ye <helmut.ye@yoozi.cn>
2016-01-16 02:05:20 +0800

    tag parser

--- app/Services/AdService.php
+++ app/Services/AdService.php
@@ -4,15 +4,15 @@ namespace App\Services;
 
 
 use App\User;
+use Cache;
 use DB;
 use App;
 
 class AdService
 {
 
-    public function show($slug)
+    public function parse($slug)
     {
-
 
         $page = App\Page::whereSlug($slug)->first();
 
@@ -27,6 +27,20 @@ class AdService
         $content = preg_replace_callback('[store-(\w*)=(\d*)]',"store_info",$content);
 
         return $content;
+    }
+
+    public function show($slug)
+    {
+        return Cache::remember(
+            'AD-SLUG-'.$slug,
+            10,
+            function () use ($slug)
+            {
+                return call_user_func_array(
+                    [$this,'parse'],[$slug]
+                )?:0;
+            }
+        );
     }
 
 }
