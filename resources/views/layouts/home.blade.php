91607f1e96c69923271f18487db5b2da9613b940
Helmut Ye <helmut.ye@yoozi.cn>
2016-01-24 00:56:21 +0800

    card

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -32,7 +32,7 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="/stores">All Stores <span class="sr-only">(current)</span></a></li>
-                <li><a href="/coupons">Coupons</a></li>
+                <li><a href="/pages/coupons.html">Coupons</a></li>
                 <li><a href="/pages/helps.html">Helps</a></li>
                 <li><a href="/home/contactus">Contact Us</a></li>
 
